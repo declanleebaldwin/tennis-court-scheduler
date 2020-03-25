3f3d75309df093bb2a3926c6a1f38edc6da5f6a7
declan baldwin <dbaldwin4@boxfusionconsulting.com>
2020-03-25 17:18:45 +0000

    adding sign in page with firebase auth

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,81 @@
 <template>
-    <div id="app">
-        <section class="hero is-success">
-            <div class="hero-head">
-                <header class="navbar">
-                    <div class="container">
-                        <div class="navbar-brand">
-                            <span class="navbar-burger burger" data-target="navbarMenuHeroC">
-                                <span></span>
-                                <span></span>
-                                <span></span>
-                            </span>
-                        </div>
-                        <div id="navbarMenuHeroC" class="navbar-menu">
-                            <div class="navbar-end">
-                                <a class="navbar-item is-active">
-                                    Make a Booking
-                                </a>
-                                <a class="navbar-item">
-                                    My Bookings
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </header>
-            </div>
-            <div class="hero-body">
-                <div class="container">
-                    <h1 class="title">
-                        Tennis Court Scheduler
-                    </h1>
-                    <h2 class="subtitle">
-                        Queens Club Gardens
-                    </h2>
-                </div>
-            </div>
-        </section>
-        <router-view />
-    </div>
+	<div id="app">
+		<section class="hero is-success">
+			<div class="hero-head">
+				<header class="navbar">
+					<div class="container">
+						<div class="navbar-brand">
+							<span class="navbar-burger burger" data-target="navbarMenuHeroC">
+								<span></span>
+								<span></span>
+								<span></span>
+							</span>
+						</div>
+						<div id="navbarMenuHeroC" class="navbar-menu">
+							<div class="navbar-end">
+								<a class="navbar-item is-active">
+									Make a Booking
+								</a>
+								<a class="navbar-item">
+									My Bookings
+								</a>
+							</div>
+						</div>
+					</div>
+				</header>
+			</div>
+			<div class="hero-body">
+				<div class="container">
+					<h1 class="title">
+						Tennis Court Scheduler
+					</h1>
+					<h2 class="subtitle">
+						Queens Club Gardens
+					</h2>
+				</div>
+			</div>
+		</section>
+		<router-view />
+	</div>
 </template>
-
+<script>
+import firebase from "firebase";
+export default {
+    data() {
+        return {
+            user: null,
+            authenticated: false
+        }
+    },
+    watch: {
+        user(newUser) {
+            if(newUser == null) {
+                this.authenticated = false;
+            } else {
+                this.authenticated = true;
+            }
+        }
+    },
+	mounted() {
+        let $this = this;
+		var provider = new firebase.auth.GoogleAuthProvider();
+		firebase
+			.auth()
+			.signInWithPopup(provider)
+			.then(function(result) {
+				// This gives you a Google Access Token. You can use it to access the Google API.
+				// The signed-in user info.
+                $this.user = result.user;
+				// ...
+			})
+			.catch(function(error) {
+                console.log(error);
+			});
+	}
+};
+</script>
 <style lang="css">
 .hero {
-  margin-bottom: 50px;
+	margin-bottom: 50px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,20 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './../node_modules/bulma/css/bulma.css';
+import * as firebase from 'firebase';
+import "firebase/auth";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBqSavDHySRTydr9uGwhhExWrNploNo4lk",
+  authDomain: "tennis-court-scheduler.firebaseapp.com",
+  databaseURL: "https://tennis-court-scheduler.firebaseio.com",
+  projectId: "tennis-court-scheduler",
+  storageBucket: "tennis-court-scheduler.appspot.com",
+  messagingSenderId: "897574807945",
+  appId: "1:897574807945:web:87f6a63d30dd4212e99958"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 Vue.config.productionTip = false
 
